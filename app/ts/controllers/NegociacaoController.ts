89fb1c43951c9a6a4d233fdb136dd18adc6ccfdd
Alessandro Bartsch <alessandrobartsch94@gmail.com>
2021-04-08 08:47:57 -0300

    adicionando view e pequenas modificacoes

--- app/ts/controllers/NegociacaoController.ts
+++ app/ts/controllers/NegociacaoController.ts
@@ -3,18 +3,22 @@ class NegociacaoController {
     private _inputData: HTMLInputElement;
     private _inputQuantidade: HTMLInputElement;
     private _inputValor: HTMLInputElement;
+    private _negociacoes = new Negociacoes;
+    private _negociacoesView = new NegociacoesView('#negociacoesView');
     
     constructor() {
-        this._inputData = document.querySelector('#data');
-        this._inputQuantidade = document.querySelector('#quantidade');
-        this._inputValor = document.querySelector('#valor');
+        this._inputData = <HTMLInputElement>document.querySelector('#data');
+        this._inputQuantidade = <HTMLInputElement>document.querySelector('#quantidade');
+        this._inputValor = <HTMLInputElement>document.querySelector('#valor');
+        this._negociacoesView.update();
     }
 
     adiciona(event: Event) {
         event.preventDefault();
 
         const negociacao = new Negociacao(new Date(this._inputData.value.replace(/-/g, ',')), parseInt( this._inputQuantidade.value), parseInt(this._inputValor.value));
-        console.log(negociacao);
+
+        this._negociacoes.adiciona(negociacao);
     }
     
 }
